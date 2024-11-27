ba8ea730fb2bc48c8faf6a70f1844e9289dcf8d6
Nicolas Fella <nicolas.fella@kdab.com>
2024-11-27 19:41:57 +0100

    Don't use signDocumentWithAppearance for SignatureAnnotation
    The annotation already has an appearance, so avoid creating
    more and thus writing more stuff to the document

--- qt6/src/poppler-annotation.cc
+++ qt6/src/poppler-annotation.cc
@@ -3097,7 +3097,7 @@ SignatureAnnotation::SigningResult SignatureAnnotation::sign(const QString &outp
     Q_D(SignatureAnnotation);
     auto formField = std::make_unique<FormFieldSignature>(d->parentDoc, d->pdfPage, static_cast<::FormWidgetSignature *>(d->field->getCreateWidget()));
 
-    const auto result = formField->sign(outputFileName, data);
+    const auto result = formField->signWithoutAppearance(outputFileName, data);
 
     switch (result) {
     case FormFieldSignature::SigningSuccess:
